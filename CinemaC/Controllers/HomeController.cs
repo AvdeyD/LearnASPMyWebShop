b876ff1bbcca700bdea77e87334dc7d53dd83f9a
Denis <avdey@mail.ru>
2020-07-21 23:50:22 +0300

    Comment

--- CinemaC/Controllers/HomeController.cs
+++ CinemaC/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace CinemaC.Controllers
         {
             return View();
         }
-
+        //
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
