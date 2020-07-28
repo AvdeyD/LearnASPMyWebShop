09ab874c3951ec18a8b8217873fc9d87fe3df724
Denis <avdey@mail.ru>
2020-07-29 00:03:38 +0300

    2

---
+++ CinemaC/Controllers/CinemaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaC.Controllers
+{
+    public class CinemaController : Controller
+    {
+        // GET: Cinema
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
