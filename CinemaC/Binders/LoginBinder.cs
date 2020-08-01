3bc7ab736df89fbf46e8b7440d023bbad1ff37f6
Denis <avdey@mail.ru>
2020-08-02 02:01:18 +0300

    Login start lesson

---
+++ CinemaC/Binders/LoginBinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+using CinemaC.Models;
+using IModelBinder = System.Web.Mvc.IModelBinder;
+using ModelBindingContext = System.Web.Mvc.ModelBindingContext;
+
+namespace CinemaC.Binders
+{
+    public class LoginBinder: IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var model = new LoginModel
+            {
+                Login = controllerContext.HttpContext.Request.Form["Login"],
+                Password = controllerContext.HttpContext.Request.Form["Password"]
+            };
+            return model;
+        }
+    }
+}
---
+++ CinemaC/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CinemaC.Models;
+
+namespace CinemaC.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        [HttpGet]// GET: Account}
+        public ActionResult Login()
+        {
+            var model = new LoginModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("LoginResult");
+            }
+            return View();
+        }
+    }
+}
