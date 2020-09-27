e4b4c3c793fbb2c674ccc1f58bf4e1a29ad6677d
Denis <avdey@mail.ru>
2020-09-27 23:49:50 +0300

    sql education

---
+++ CinemaC/Profiles/SqlDataReaderProfile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using AutoMapper;
+using CinemaC.Models.Domain;
+
+namespace CinemaC.Profiles
+{
+    public class SqlDataReaderProfile:Profile
+    {
+        public SqlDataReaderProfile()
+        {
+            CreateMap<SqlDataReader, Movie>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src["Id"]))
+                .ForMember(dst => dst.Title, opt => opt.MapFrom(src => src["Title"]))
+                .ForMember(dst => dst.Description, opt => opt.MapFrom(src => src["Description"]))
+                .ForMember(dst => dst.MinAge, opt => opt.MapFrom(src => src["MinAge"]))
+                .ForMember(dst => dst.Duration, opt => opt.MapFrom(src => src["Duration"]))
+                .ForMember(dst => dst.Rating, opt => opt.MapFrom(src => src["Rating"]))
+                .ForMember(dst => dst.ImgUrl, opt => opt.MapFrom(src => src["ImgUrl"]))
+                .ForMember(dst => dst.Director, opt => opt.MapFrom(src => src["Director"]))
+                .ForMember(dst => dst.ReleaseDate, opt => opt.MapFrom(src => src["ReleaseDate"]))
+                .ForMember(dst => dst.Genres, opt => opt.Ignore())
+                .AfterMap((src, dst) =>
+                {
+                    var genres = (string)src["Genres"];
+                    if (!string.IsNullOrWhiteSpace(genres))
+                    {
+                        var parsedGenres = genres.Split(',').Select(x => (Genre) Enum.Parse(typeof(Genre), x));
+                        dst.Genres = parsedGenres.ToArray();
+                    }
+                })
+                .ForAllOtherMembers(x=>x.Ignore());
+        }
+    }
+}
---
+++ CinemaC/Services/EntityTicketsServise.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using CinemaC.DataBaseLayer;
+using CinemaC.Interfaces;
+using CinemaC.Models.Domain;
+using CinemaC.Models.Tickets;
+
+namespace CinemaC.Services
+{
+    public class EntityTicketsServise: ITicketService
+    {
+        public MovieListItem[] GetFullMoviesInfo()
+        {
+            var model = new List<MovieListItem>();
+            using (var context = new CinemaContext())
+            {
+                foreach (var movie in context.Movies.Where(x => !x.IsDeleted).ToArray())
+                {
+                    var movieListItem = new MovieListItem
+                    {
+                        Movie = movie,
+                        AvailableTimeSlots = context.TimeSlots
+                            .Where(x => x.MovieId == movie.Id)
+                            .Select(x => new TimeSlotTag
+                            {
+                                Cost = x.Cost,
+                                StartTime = x.StarTime,
+                                TimeSlotId = x.Id
+                            })
+                            .ToArray()
+                    };
+                    model.Add(movieListItem);
+                }
+            }
+
+            return model.ToArray();
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            using (var context = new CinemaContext())
+            {
+                return context.Movies.Where(x => !x.IsDeleted).First(x => x.Id==id);
+            }
+        }
+
+        public Movie[] GetAllMovies()
+        {
+            using (var context = new CinemaContext())
+            {
+                return context.Movies.Where(x=>!x.IsDeleted).ToArray();
+            }
+        }
+
+        public bool UpdateMovie(Movie movie)
+        {
+            using (var context = new CinemaContext())
+            {
+                context.Movies.AddOrUpdate(movie);
+                context.SaveChanges();
+            }
+
+            return true;
+        }
+
+        public bool RemoveMovie(int id)
+        {
+            using (var context = new CinemaContext())
+            {
+                var movie = context.Movies.First(x => x.Id == id);
+                movie.IsDeleted = true;
+                context.SaveChanges();
+            }
+
+            return true;
+        }
+
+        public Hall GetHallById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Hall[] GetAllHalls()
+        {
+            using (var context = new CinemaContext())
+            {
+               return  context.Halls.ToArray();
+            }
+        }
+
+        public bool UpdateHall(Hall movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TimeSlot GetTimeSlotById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TimeSlot[] GetTimeSlotByMovieId(int id)
+        {
+            using (var context = new CinemaContext())
+            {
+                return context.TimeSlots.Where(x => x.MovieId == id).ToArray();
+            }
+        }
+
+        public TimeSlot[] GetAllTimeSlots()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateTimeSlot(TimeSlot movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public TimeSlotTag[] GetTimeSlotTagsByMovieId(int movieId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CreateMovie(Movie newMovie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CreateTimeSlot(TimeSlot newTimeSlot)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CreateHall(Hall newHall)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool AddRequestedSeatsToTimeSlot(SeatsProcessRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
